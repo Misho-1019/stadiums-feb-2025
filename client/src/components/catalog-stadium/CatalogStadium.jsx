c9be3eae67ac5ab4d08c5b7790caa141f8ad53c9
Misho1019 <mikailtorres99@gmail.com>
2025-03-15 02:51:20 +0200

    Empty stadiums screen

--- client/src/components/catalog-stadium/CatalogStadium.jsx
+++ client/src/components/catalog-stadium/CatalogStadium.jsx
@@ -6,17 +6,17 @@ export default function CatalogStadium() {
     const [stadiums, setStadiums] = useState([])
     useEffect(() => {
         stadiumService.getAll()
-           .then(setStadiums)
+            .then(setStadiums)
     }, [])
-    
+
     return (
         <section id="catalog-page">
             <h1>All Stadiums</h1>
-            {/* Display div: with information about every game (if any) */}
-            
-            {stadiums.map(stadium => <ItemCatalogStadium key={stadium._id} {...stadium} />)}
-            {/* Display paragraph: If there is no games */}
-            <h3 className="no-articles">No stadiums yet</h3>
+
+            {stadiums.length > 0
+                ? stadiums.map(stadium => <ItemCatalogStadium key={stadium._id} {...stadium} />)
+                : <h3 className="no-articles">No stadiums yet</h3>
+            }
         </section>
     )
 }
